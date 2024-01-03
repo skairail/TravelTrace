ccf0b4334adcd05fdb366136ee7141d76d4f5dbc
Oleksandr <skairail1600@gmail.com>
2024-01-03 16:35:46 +0200

    style: Added basic CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
@@ -15,6 +16,8 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/pageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
+      <AppNav />
       <PageNav />
-      <h1>TravelTrace</h1>;
+      <h1>TravelTrace</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
