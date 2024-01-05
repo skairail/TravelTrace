c535b494d9bde165d73833aea6e7089bc85507a2
Oleksandr <skairail1600@gmail.com>
2024-01-05 19:02:13 +0200

    refactor : components folder refactoring

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Button from "./Button";
+import Button from "../Button/Button";
 
 function BackButton() {
   const navigate = useNavigate();
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from "./Sidebar.module.css";
+// import styles from "./Sidebar/Sidebar.module.css";
+import styles from "../Sidebar/Sidebar.module.css";
 
 export default function Footer() {
   return (
